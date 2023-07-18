5b2ae831de5af6d7a29010b338628be02e93df16
Stefan Rizescu <iulian.stefan.rizescu@gmail.com>
2023-07-18 18:16:26 +0300

    lint

--- src/lib/i18n/check-translations.ts
+++ src/lib/i18n/check-translations.ts
@@ -1,13 +1,17 @@
-export const checkTranslations = (source: Record<string, string>, other: Record<string, string>, otherName: string) => {
+export const checkTranslations = (
+	source: Record<string, string>,
+	other: Record<string, string>,
+	otherName: string,
+) => {
 	const sourceKeys = Object.keys(source);
 	const otherKeys = Object.keys(other);
 
-  const diff = sourceKeys.filter(k => !otherKeys.includes(k));
-  if (diff.length === 0) return;
-	
-  throw new Error(`Translations are different! ${otherName} is missing content:
+	const diff = sourceKeys.filter((k) => !otherKeys.includes(k));
+	if (diff.length === 0) return;
+
+	throw new Error(`Translations are different! ${otherName} is missing content:
   - ${diff.join("\n  - ")}
   
   Please check the files in \`src/content/i18n\` for differences
   `);
-}
+};
