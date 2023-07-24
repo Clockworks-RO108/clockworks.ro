631ac5b6c85d3a88b95812d269989e9c1b5cb24b
Stefan Rizescu <iulian.stefan.rizescu@gmail.com>
2023-07-24 22:56:16 +0300

    move config to designated folder

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,5 @@
-import { defineCollection, z } from "astro:content";
-import en from "./i18n/en.json";
-
-type Keys = Array<keyof typeof en>;
-type Content = {
-	[K in Keys[number]]: z.ZodString;
-};
-
-const keys = Object.keys(en) as Keys;
-const i18nSchema = z.object(
-	keys.reduce((acc: Content, curr: Keys[number]) => {
-		acc[curr] = z.string();
-		return acc;
-	}, {} as Content),
-);
+import { i18n } from "./i18n/_config";
 
 export const collections = {
-	i18n: defineCollection({
-		type: "data",
-		schema: i18nSchema,
-	}),
+	i18n,
 };
