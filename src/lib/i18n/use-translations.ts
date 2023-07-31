8440e47be831ed0733686eb12faf8f335be74926
Stefan Rizescu <iulian.stefan.rizescu@gmail.com>
2023-07-31 09:02:04 +0300

    simplify usetranslations call

--- src/lib/i18n/use-translations.ts
+++ src/lib/i18n/use-translations.ts
@@ -1,26 +1,37 @@
 import { getCollection } from "astro:content";
 import type { CollectionEntry } from "astro:content";
-import { isLocale } from "./slugs";
 import { locales } from "./locales";
 import type { Locale } from "./locales";
 import { checkTranslations } from "./check-translations";
 
 import en from "~/content/i18n/en.json";
 import ro from "~/content/i18n/ro.json";
+import { currentLocaleAtom } from "../current-url";
 
 checkTranslations(en, ro, "`ro.json`");
 
+type Keys = keyof CollectionEntry<"i18n">["data"];
+
 let translations: Record<Locale, CollectionEntry<"i18n">["data"]>;
 try {
+	const collection = await getCollection("i18n");
 	translations = Object.fromEntries(
-		(await getCollection("i18n")).map((t) => [t.id, t.data] as const),
+		collection.map((t) => [t.id, t.data] as const),
 	) as Record<Locale, CollectionEntry<"i18n">["data"]>;
 } catch {}
 
-export const useTranslations = (lang: string | undefined) => {
-	const locale = isLocale(lang) ? lang : "en";
+// I keep it just in case
+// export const useTranslations = (lang: string | undefined) => {
+// 	const locale = isLocale(lang) ? lang : "en";
+// 	// const locale = currentLocaleAtom.get();
 
-	return (key: keyof CollectionEntry<"i18n">["data"]) => translations[locale][key];
+// 	return (key: keyof CollectionEntry<"i18n">["data"]) => translations[locale][key];
+// };
+
+export const useTranslations = () => {
+	const locale = currentLocaleAtom.get();
+
+	return (key: Keys) => translations[locale][key];
 };
 
 export const generateStaticPaths = () => {
