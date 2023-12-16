b3a049c889dfd16e989c4e92aa73ab4414c67e91
Stefan Rizescu <iulian.stefan.rizescu@gmail.com>
2023-12-16 16:25:02 +0200

    refactor forms schema

--- src/lib/forms/api.ts
+++ src/lib/forms/api.ts
@@ -4,15 +4,23 @@ import type { Locale } from "../i18n";
 
 export * as formsApi from "./api";
 
-const mapData = (locale: Locale) => (form: CollectionEntry<"forms">) => ({
-	id: form.id,
-	height: form.data.height,
-	image: form.data.image,
-	...form.data[locale],
-});
+const mapData = (locale: Locale) => (form: CollectionEntry<"forms">) => {
+	return {
+		id: form.id,
+		height: form.data.height,
+		image: form.data.image,
+		...((
+			form.data.data as {
+				[K in Locale]: { url: string; title: string; description: string };
+			}
+		)[locale] ?? { url: null }),
+	};
+};
 
 export const getAll = async (locale: Locale) =>
-	(await getCollection("forms")).map(mapData(locale));
+	(await getCollection("forms"))
+		.filter((form) => form.data.data.lang === "both" || form.data.data.lang == locale)
+		.map(mapData(locale));
 
 export const getById = async (id: CollectionEntry<"forms">["id"], locale: Locale) =>
 	mapData(locale)(await getDataEntryById("forms", id));
